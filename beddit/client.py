4cb4354e8a861aa6d84a0cfd9fce63efc77976f1
giginet <giginet.net@gmail.com>
2016-08-14 23:12:32 +0900

    Initial Commit

---
+++ beddit/client.py
@@ -0,0 +1,32 @@
+import requests
+from urllib.parse import urljoin
+
+
+class BedditClient(object):
+    BASE_URL = 'https://cloudapi.beddit.com'
+    access_token = None
+    user_id = None
+
+    def __init__(self, username, password):
+        payload = {
+            'grant_type': 'password',
+            'username': username,
+            'password': password
+        }
+        endpoint = urljoin(self.BASE_URL, '/api/v1/auth/authorize')
+        r = requests.post(endpoint, data=payload)
+        if r.status_code == 200:
+            response_object = r.json()
+            self.access_token = response_object['access_token']
+            self.user_id = response_object['user']
+
+    def _request_with_header(self, path, params = {}):
+        endpoint = urljoin(self.BASE_URL, path)
+        r = requests.get(endpoint,
+                         params=params,
+                         headers={'Authorization': "UserToken {}".format(self.access_token)})
+        return r
+
+    def get_sleep(self):
+        path = "/api/v1/user/{}/sleep".format(self.user_id)
+        r = self._request_with_header(path)
