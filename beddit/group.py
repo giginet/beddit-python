80ef529eaee449c4192eb58ad9a79f2df64272f1
giginet <giginet.net@gmail.com>
2016-08-16 23:58:06 +0900

    Add tests for Group model

--- beddit/group.py
+++ beddit/group.py
@@ -5,13 +5,13 @@ from .user import User
 class PendingInvite(object):
     def __init__(self, response_object):
         self.created = datetime.utcfromtimestamp(response_object['created'])
-        self.created_user_id = response_object['created_by']
+        self.created_by = response_object['created_by']
         self.email = response_object['email']
 
 
 class Group(object):
     def __init__(self, response_object):
-        self.id = response_object['id']
+        self.id = int(response_object['id'])
         self.created = datetime.utcfromtimestamp(response_object['created'])
         self.members = [User(user) for user in response_object['members']]
         self.pending_invites = [PendingInvite(invitation) for invitation in response_object['pending_invites']]
---
+++ tests/test_group.py
@@ -0,0 +1,26 @@
+import unittest
+import json
+import os
+from datetime import datetime
+from beddit.group import Group
+
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+class UserTest(unittest.TestCase):
+    @property
+    def group_response(self):
+        return json.load(open(os.path.join(BASE_DIR, 'fixtures/group.json')))
+
+    def test_group(self):
+        group = Group(self.group_response)
+        self.assertEqual(group.id, 1234)
+        self.assertEqual(group.created, datetime.utcfromtimestamp(1371472503.646541))
+        self.assertEqual(len(group.members), 1)
+        self.assertEqual(len(group.pending_invites), 1)
+
+        pending_invite = group.pending_invites[0]
+        self.assertEqual(pending_invite.created, datetime.utcfromtimestamp(1371472503.646541))
+        self.assertEqual(pending_invite.created_by, 132)
+        self.assertEqual(pending_invite.email, 'example@beddit.com')
